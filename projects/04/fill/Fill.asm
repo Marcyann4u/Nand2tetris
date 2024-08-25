f823d268b96bc7767de82ac3d9c9e1319b12c9f6
Marcyann4u <Marcelyjoana1227@gmail.com>
2024-08-25 09:06:19 -0300

    projeto 4 de nandinho

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,52 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// addr = SCREEN
+
+// i = 0
+
+// 256 x 512
+
+
+(START)
+    @SCREEN
+    D=A
+    @i
+    M=D // i = tamanho da tela
+
+(LOOP)
+    // se passou do tamanho da tela volta pro inicio
+    @i
+    D=M
+    @24575
+    D=A-D
+    @START
+    D;JLT // i < screen - 1
+
+    @KBD //teclado
+    D=M
+    @BLACKEN //se for diferente de 0 pinta de preto
+    D;JNE 
+
+    // clear the screen (write every pixel white) 
+
+    @i //pega a posição de memoria atual
+    D=M 
+    A=D  // muda o conteudo da posicao de memoria
+    M=0 //Draw white
+    @CONTINUE
+    0;JMP
+
+(BLACKEN)
+    @i //pega a posição de memoria atual
+    D=M 
+    A=D  // muda o conteudo da posicao de memoria
+    M=-1 //Draw in black
+
+
+(CONTINUE)
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,74 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// multiplicando = R0
+// multiplicador = R1
+// produto = 0
+// i = 1
+//LOOP:
+//  if i > multiplicador vapara a puta que te pariu
+//      produto = multiplicando + produto
+//       i = i + 1
+//  vapara a puta que te pariu:
+// R2 = produto
+        // Olá, Marcely do futuro, boa sorte em entender essa porra
+
+        // Declarando variavel multiplicando
+        @R0
+        D=M
+        @multiplicando
+        M=D
+
+        // declarando multiplicador
+        @R1
+        D=M
+        @multiplicador
+        M=D
+
+        @i
+        M=1
+
+        @produto
+        M=0
+
+        // perguntas frequentes:
+        // o que diabos é o D?
+        // o D guarda os dados na memória
+        // o que é o M?
+        // M é uma memória temporaria que registra o valor que está sendo utilizado, ele representa tudo, desde os endereços, às variáveis e tudo.
+        // Mas como krls ele tá guardando o valor se vc coloca só M=0?
+        // Só Deus sabe mas EU ACHO que é pq guarda numa variável
+
+(LOOP) // Criando uma função, pode ser qualquer nome :)
+        @i //aponta o local na memória que eu quero acessar
+        D=M // acessa o local apontado
+        @multiplicador
+        D=D-M // 
+        @STOP
+        D;JGT // if i > multiplicador vai para STOP
+
+        // perguntas frequentes?
+        // oq diacho é JGT?
+        // é JUMP IF GREATER THAN
+        // ou em português, quica se for maior
+
+        @produto
+        D=M
+        @multiplicando
+        D=D+M
+        @produto
+        M=D // produto = multiplicando + produto
+
+        @i
+        M=M+1 // i = i + 1
+        @LOOP // aponta pro endereço
+        0;JMP // pula independente dos desejos dele pro inicio do endereço apontado anteriormente
+(STOP)
+        @produto // referencia o productum
+        D=M 
+        @R2 // onde q a gente vai guardar o resultado final do productum
+        M=D // e bota no local desejado
+(END)
+        @END
+        0;JMP
